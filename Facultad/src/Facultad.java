0224382e1098427f08a212b7465f842afb191a34
LatynM <111091315+LatynM@users.noreply.github.com>
2022-09-26 20:21:58 -0300

    si

--- Facultad/src/Facultad.java
+++ Facultad/src/Facultad.java
@@ -1,3 +1,26 @@
+import java.util.List;
+
 public class Facultad {
+    String nombre;
+    List<Carrera> carreras;
+
+    public void agregarCarrera(Carrera carrera){
+        carreras.add(carrera);
+    }
+
+    public void eliminarCarrera(String nombreCarrera){
+        for (Carrera carrera:carreras){
+            if (carrera.getNombre().equals(nombreCarrera)){
+                carreras.remove(carrera);
+            }
+        }
+    }
+
+    public void eliminarEstudiante(String nombreEstudiante){
+        for{
+
+        }
+    }
+
 
 }

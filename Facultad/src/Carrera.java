a3511a5e17438317ac2e4076fc1692dad4ea348d
Dihue <dihue21@gmail.com>
2022-09-26 00:42:45 -0300

    Clase Carrera

--- Facultad/src/Carrera.java
+++ Facultad/src/Carrera.java
@@ -1,3 +1,58 @@
-public class Carrera {
+import java.util.List;
 
+public class Carrera {
+	// Atributos
+	private String nombre;
+	private List<Materia> materias;
+
+	public void agregarMateria(Materia materia) {
+		materias.add(materia);
+	}
+
+	// Métodos propios
+	public void eliminarMateria(String nombreMateria) {
+		for (Materia materia: materias) {
+			if (materia.getNombre().equals(nombreMateria)) {
+				materias.remove(materia);
+			}
+		}
+	}
+
+	public void encontrarMateria(String nombreMateria) {
+		for (Materia materia: materias) {
+			if (materia.getNombre().equals(nombreMateria)) {
+				System.out.println("Materia encontrada");
+			}
+		}
+	}
+
+	// Constructor
+	public Carrera(String nombre, List<Materia> materias) {
+		this.nombre = nombre;
+		this.materias = materias;
+	}
+
+	// Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setMaterias(List<Materia> materias) {
+		this.materias = materias;
+	}
+
+	public List<Materia> getMaterias() {
+		return materias;
+	}
+
+	// Salida para todos los datos
+	@Override
+	public String toString() {
+		return "\n|--> " + nombre + "\n" +
+			"\tMaterias: "  + materias;
+	}
 }
